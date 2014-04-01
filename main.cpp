084530e215de05f5c249f6af15386c61b8fd9c92
Yiming Shen <soimy@163.com>
2014-04-01 18:11:42 +0800

    WIP parallel detection interval

--- main.cpp
+++ main.cpp
@@ -1,14 +1,40 @@
 #include <opencv2/opencv.hpp>
 #include <opencv2/highgui/highgui.hpp>
+#include <pthread.h>
 
 #include <stdio.h>  
 #include <iostream>
 #include <string.h>
+#include <functional>
+#include <chrono>
+#include <future>
+#include <cstdio>
 
 using namespace std;
 using namespace cv;
 
+// Timer implemention
+class later{
+    public:
+        template <class callable, class... arguments>  later(int after, bool async, callable&& f, arguments&&... args){
+            function<typename result_of<callable(arguments...)>::type()> task(bind(forward<callable>(f), forward<arguments>(args)...));
+            if (async){
+                thread([after, task]() {
+                    this_thread::sleep_for(std::chrono::milliseconds(after));
+                    task();
+                }).detach();
+            }
+            else{
+                this_thread::sleep_for(std::chrono::milliseconds(after));
+                task();
+            }
+        }
+};
+
+// setting for capcam global viarables
+vector<Rect> objects;
 static CascadeClassifier cascade;
+static int sampleRate = 1000; // in ms
 static CvScalar colors[] =
 {
 	{ { 0, 0, 255 } },
@@ -20,12 +46,11 @@ static CvScalar colors[] =
 	{ { 255, 0, 0 } },
 	{ { 255, 0, 255 } }
 };
+string cascade_name = "data/haarcascade_frontalface_alt.xml";
 
-vector<Rect> objects;
-
-void detectObj(Mat srcFrame);
-
-string cascade_name = "haarcascade_frontalface_alt.xml";
+// Declare of functions
+void detectObj(Mat& srcFrame);
+void updateGraph();
 
 int main(int argc, char** argv)
 {
@@ -50,6 +75,9 @@ int main(int argc, char** argv)
 	Mat frame;
 	namedWindow("windata");
 
+    // Start the detect timer
+    later(sampleRate, true, &detectObj, frame);
+    
 	while (true){
 		cap >> frame;
 		if (frame.empty())
@@ -58,29 +86,37 @@ int main(int argc, char** argv)
 		for (vector<cv::Rect>::iterator r = objects.begin(); r != objects.end(); ++r)
 			rectangle(frame, *r, colors[(r - objects.begin())%8], 1, 8, 1);
 		imshow("windata",frame);
+        int keyCode = waitKey(20);
+        if (keyCode == 'q' || keyCode == 'Q')
+            break;
 	}
 	return 0;
 }
 
-void detectObj(Mat srcframe)
+void detectObj(Mat& srcframe)
 {
-	double scale = 1.1;
-	cvtColor(srcframe, srcframe, CV_BGR2GRAY);
-	resize(srcframe, srcframe, Size(256, 256));
-	equalizeHist(srcframe, srcframe);
+    
+    if (srcframe.empty())
+        return;
+	
+    double scale = 1.1;
+    Mat smallFrame;
+	cvtColor(srcframe, smallFrame, CV_BGR2GRAY);
+	resize(smallFrame, smallFrame, Size(256, 256));
+	equalizeHist(smallFrame, smallFrame);
 
 	if (cascade.empty())
 		return;
 
 	double t = (double)cvGetTickCount(); // start evaluating process time
-	cascade.detectMultiScale(srcframe, objects,scale,3, 
+	cascade.detectMultiScale(smallFrame, objects,scale,3,
 					0|CV_HAAR_SCALE_IMAGE,
 					Size(50,50), Size(200,200) );
 	t = (double)cvGetTickCount() - t;
-	//printf("detection time = %gms/n", t / ((double)cvGetTickFrequency()*1000.));
+	printf("detection time = %gms/n", t / ((double)cvGetTickFrequency()*1000.));
 
 	// Display detect time cost on screen overlay
-	displayOverlay("windata", "detection time = " + to_string(t / ((double)cvGetTickFrequency()*1000.)) + "ms");
+    //	displayOverlay("windata", "detection time = " + to_string(t / ((double)cvGetTickFrequency()*1000.)) + "ms");
 
 	return;
 }
