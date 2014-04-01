f2501f561aa1ee497ea977e48065f3eb125eca9d
Yiming Shen <soimy@163.com>
2014-04-01 21:32:44 +0800

    done rough threaded detection

--- main.cpp
+++ main.cpp
@@ -5,10 +5,8 @@
 #include <stdio.h>  
 #include <iostream>
 #include <string.h>
-#include <functional>
 #include <chrono>
 #include <future>
-#include <cstdio>
 
 using namespace std;
 using namespace cv;
@@ -16,12 +14,21 @@ using namespace cv;
 // Timer implemention
 class later{
     public:
-        template <class callable, class... arguments>  later(int after, bool async, callable&& f, arguments&&... args){
-            function<typename result_of<callable(arguments...)>::type()> task(bind(forward<callable>(f), forward<arguments>(args)...));
+        template <class callable, class... arguments>  later(int after, bool async, int loop, callable&& f, arguments&&... args){
+            function<typename std::result_of<callable(arguments...)>::type()> task(bind(forward<callable>(f), forward<arguments>(args)...));
             if (async){
-                thread([after, task]() {
-                    this_thread::sleep_for(std::chrono::milliseconds(after));
-                    task();
+                thread([after, task, loop]() {
+                    if (loop == 0) {
+                        while (true) {
+                            this_thread::sleep_for(std::chrono::milliseconds(after));
+                            task();
+                        }
+                    }else{
+                        for (int i = loop; i >= 0; --i) {
+                            this_thread::sleep_for(std::chrono::milliseconds(after));
+                            task();
+                        }
+                    }
                 }).detach();
             }
             else{
@@ -34,7 +41,7 @@ class later{
 // setting for capcam global viarables
 vector<Rect> objects;
 static CascadeClassifier cascade;
-static int sampleRate = 1000; // in ms
+static int sampleRate = 200; // in ms
 static CvScalar colors[] =
 {
 	{ { 0, 0, 255 } },
@@ -47,7 +54,7 @@ static CvScalar colors[] =
 	{ { 255, 0, 255 } }
 };
 string cascade_name = "data/haarcascade_frontalface_alt.xml";
-
+Mat frame;
 // Declare of functions
 void detectObj(Mat& srcFrame);
 void updateGraph();
@@ -72,20 +79,21 @@ int main(int argc, char** argv)
 	cap.set(CV_CAP_PROP_FRAME_WIDTH, 640);
 	cap.set(CV_CAP_PROP_FRAME_HEIGHT, 480);
 	
-	Mat frame;
 	namedWindow("windata");
 
     // Start the detect timer
-    later(sampleRate, true, &detectObj, frame);
+    later(sampleRate, true, 0, &detectObj, frame);
     
 	while (true){
 		cap >> frame;
 		if (frame.empty())
 			continue;
+        Mat dispFrame(frame.size(),CV_8UC3);
+        dispFrame = frame;
 		if (!objects.empty())
-		for (vector<cv::Rect>::iterator r = objects.begin(); r != objects.end(); ++r)
-			rectangle(frame, *r, colors[(r - objects.begin())%8], 1, 8, 1);
-		imshow("windata",frame);
+            for (vector<cv::Rect>::iterator r = objects.begin(); r != objects.end(); ++r)
+                rectangle(dispFrame, *r, colors[(r - objects.begin())%8], 1, 8, 0);
+		imshow("windata",dispFrame);
         int keyCode = waitKey(20);
         if (keyCode == 'q' || keyCode == 'Q')
             break;
@@ -96,13 +104,15 @@ int main(int argc, char** argv)
 void detectObj(Mat& srcframe)
 {
     
-    if (srcframe.empty())
+    if (frame.empty()){
+        printf("No input frames\n");
         return;
+    }
 	
     double scale = 1.1;
-    Mat smallFrame;
-	cvtColor(srcframe, smallFrame, CV_BGR2GRAY);
-	resize(smallFrame, smallFrame, Size(256, 256));
+    Mat smallFrame(cv::Size(640,480), CV_8UC1);
+	cvtColor(frame, smallFrame, CV_BGR2GRAY);
+//	resize(smallFrame, smallFrame, Size(256, 256));
 	equalizeHist(smallFrame, smallFrame);
 
 	if (cascade.empty())
@@ -110,13 +120,10 @@ void detectObj(Mat& srcframe)
 
 	double t = (double)cvGetTickCount(); // start evaluating process time
 	cascade.detectMultiScale(smallFrame, objects,scale,3,
-					0|CV_HAAR_SCALE_IMAGE,
+					CV_HAAR_SCALE_IMAGE|CV_HAAR_DO_CANNY_PRUNING,
 					Size(50,50), Size(200,200) );
 	t = (double)cvGetTickCount() - t;
-	printf("detection time = %gms/n", t / ((double)cvGetTickFrequency()*1000.));
-
-	// Display detect time cost on screen overlay
-    //	displayOverlay("windata", "detection time = " + to_string(t / ((double)cvGetTickFrequency()*1000.)) + "ms");
+    printf("detection time = %gms\n", t / ((double)cvGetTickFrequency()*1000.));
 
 	return;
 }
